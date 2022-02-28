c34f2b1ae87201498a4fb862c8e291d7c5dae45f
ilya-24 <ilya-balon@mail.ru>
2022-03-01 01:36:49 +1000

    Updates 1

---
+++ src/Cat.java
@@ -0,0 +1,8 @@
+public class Cat {
+    public String name;
+    public int weight = 3000;
+    public int age = 0;
+    public int hungry = 0;
+    public int wantPlay = 0;
+
+    }
---
+++ src/CatLife.java
@@ -0,0 +1,5 @@
+public class CatLife {
+    public static void main(String[] args) {
+
+    }
+}
